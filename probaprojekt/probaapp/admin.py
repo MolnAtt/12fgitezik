f1f6a7b6b781cd7ff0fb9978999b2e550fe17997
MolnarAttila <MolnarAttila@tan-nt1.tanari.szlgbp.local>
2020-12-09 14:29:39 +0100

    modellek kialakítása és migration

--- probaprojekt/probaapp/admin.py
+++ probaprojekt/probaapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import User, Tetel, Kapcsolat
+
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Tetel)
+admin.site.register(Kapcsolat)
--- probaprojekt/probaapp/models.py
+++ probaprojekt/probaapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+	nev = models.CharField(max_length=100)
+	jelszo = models.CharField(max_length=100)
+	def __str__(self):
+		return self.nev + "(" + str(self.id) +")"
+
+class Tetel(models.Model):
+	nev = models.CharField(max_length=100)
+	megj = models.CharField(max_length=255)
+	def __str__(self):
+		return self.nev # + "(" + self.megj +")"
+
+class Kapcsolat(models.Model):
+	userid = models.ForeignKey(User, on_delete=models.CASCADE)
+	tetelid = models.ForeignKey(Tetel, on_delete=models.CASCADE)
+	def __str__(self):
+		return f"{self.userid.nev} ({self.userid.id}) --> {self.tetelid.nev}"
